60dcd71033d937f170b13071db17f8136c7fcb2b
Kjell Arne Hellum <kjell-arne.hellum@capgemini.com>
2019-05-10 00:52:28 +0200

    credited kskjer for free hints in about

--- fAbout.Designer.cs
+++ fAbout.Designer.cs
@@ -36,9 +36,10 @@
             // 
             // lAboutText
             // 
-            this.lAboutText.Location = new System.Drawing.Point(27, 33);
+            this.lAboutText.Location = new System.Drawing.Point(36, 41);
+            this.lAboutText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lAboutText.Name = "lAboutText";
-            this.lAboutText.Size = new System.Drawing.Size(308, 145);
+            this.lAboutText.Size = new System.Drawing.Size(411, 206);
             this.lAboutText.TabIndex = 0;
             this.lAboutText.Text = resources.GetString("lAboutText.Text");
             this.lAboutText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -47,8 +48,9 @@
             // 
             this.lAboutTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lAboutTitle.Location = new System.Drawing.Point(0, 1);
+            this.lAboutTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lAboutTitle.Name = "lAboutTitle";
-            this.lAboutTitle.Size = new System.Drawing.Size(359, 27);
+            this.lAboutTitle.Size = new System.Drawing.Size(479, 33);
             this.lAboutTitle.TabIndex = 1;
             this.lAboutTitle.Text = "Majora\'s Mask Randomizer";
             this.lAboutTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -56,9 +58,10 @@
             // discordLinkLabel
             // 
             this.discordLinkLabel.AutoSize = true;
-            this.discordLinkLabel.Location = new System.Drawing.Point(108, 119);
+            this.discordLinkLabel.Location = new System.Drawing.Point(144, 146);
+            this.discordLinkLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.discordLinkLabel.Name = "discordLinkLabel";
-            this.discordLinkLabel.Size = new System.Drawing.Size(141, 13);
+            this.discordLinkLabel.Size = new System.Drawing.Size(179, 17);
             this.discordLinkLabel.TabIndex = 2;
             this.discordLinkLabel.TabStop = true;
             this.discordLinkLabel.Text = "https://discord.gg/8qbreUM";
@@ -67,14 +70,15 @@
             // 
             // fAbout
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(359, 183);
+            this.ClientSize = new System.Drawing.Size(479, 254);
             this.Controls.Add(this.discordLinkLabel);
             this.Controls.Add(this.lAboutTitle);
             this.Controls.Add(this.lAboutText);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "fAbout";
             this.Text = "About";
