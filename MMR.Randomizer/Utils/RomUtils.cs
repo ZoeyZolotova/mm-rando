7300a61dd8bfaba5ebc5ae59b2d631c5b8667f2c
isghj5 <isghj5@gmail.com>
2020-01-28 21:27:30 -0800

    Lowered CPU thread priority during yaz0 file re-encoding

--- MMR.Randomizer/Utils/RomUtils.cs
+++ MMR.Randomizer/Utils/RomUtils.cs
@@ -10,6 +10,7 @@ using System.IO.Compression;
 using MMR.Randomizer.Utils.Mzxrules;
 using System.Diagnostics;
 using System.Security.Cryptography;
+using System.Threading;
 
 namespace MMR.Randomizer.Utils
 {
@@ -298,6 +299,9 @@ namespace MMR.Randomizer.Utils
             {
                 if (file.IsCompressed && file.WasEdited)
                 {
+                    // lower priority so that the rando can't lock a badly scheduled CPU by using 100%
+                    var previous_thread_priority = Thread.CurrentThread.Priority;
+                    Thread.CurrentThread.Priority = ThreadPriority.Lowest;
                     byte[] result;
                     var newSize = Yaz.Encode(file.Data, file.Data.Length, out result);
                     if (newSize >= 0)
@@ -305,6 +309,8 @@ namespace MMR.Randomizer.Utils
                         file.Data = new byte[newSize];
                         ReadWriteUtils.Arr_Insert(result, 0, newSize, file.Data, 0);
                     }
+                    // this thread is borrowed, we don't want it to always be the lowest priority, return to previous state
+                    Thread.CurrentThread.Priority = previous_thread_priority;
                 }
             });
             byte[] ROM = new byte[0x2000000];
