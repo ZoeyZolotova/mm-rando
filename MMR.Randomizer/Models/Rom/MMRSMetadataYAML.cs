784a2cff090b1e4d31ab21ee9b53bc77082fdaa3
Crin <usanimere@outlook.com>
2025-04-30 08:59:43 -0700

    Create MMRSMetadataYAML.cs
    Move MMRSMetadataYAML class to its own file so it can be used between SequenceUtils and MusicConversionUtils

---
+++ MMR.Randomizer/Models/Rom/MMRSMetadataYAML.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using YamlDotNet.Serialization;
+
+namespace MMR.Randomizer.Models.Rom
+{
+    public class MMRSMetadataYAML
+    {
+        [YamlMember(Alias = "game")]
+        public string Game { get; set; }
+
+        [YamlMember(Alias = "metadata")]
+        public Meta Metadata { get; set; }
+
+        public class Meta
+        {
+            [YamlMember(Alias = "display name")]
+            public string DisplayName { get; set; }
+
+            [YamlMember(Alias = "instrument set")]
+            public string InstrumentSet { get; set; }
+
+            [YamlMember(Alias = "song type")]
+            public string SongType { get; set; }
+
+            [YamlMember(Alias = "music groups")]
+            public List<object> MusicGroups { get; set; }
+
+            [YamlMember(Alias = "audio samples")]
+            public Dictionary<string, Sample> AudioSamples { get; set; }
+
+        }
+
+        public class Sample
+        {
+            [YamlMember(Alias = "instrument type")]
+            public string Type { get; set; } = null;
+
+            [YamlMember(Alias = "list index")]
+            public int? Index { get; set; } = null;
+
+            [YamlMember(Alias = "key region")]
+            public string KeyRegion { get; set; } = null;
+
+            [YamlMember(Alias = "temp address")]
+            public uint? TempAddress { get; set; } = null;
+        }
+    }
+}
