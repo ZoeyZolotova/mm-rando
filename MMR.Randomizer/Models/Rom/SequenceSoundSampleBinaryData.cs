3d81c8a3fe181ac06bdc5f0b2359f457796d7933
Crin <usanimere@outlook.com>
2025-05-03 13:00:48 -0700

    Add InstrumentType, ListIndex, and KeyRegion to SequenceSoundSampleBinaryData

--- MMR.Randomizer/Models/Rom/SequenceSoundSampleBinaryData.cs
+++ MMR.Randomizer/Models/Rom/SequenceSoundSampleBinaryData.cs
@@ -7,5 +7,10 @@ namespace MMR.Randomizer.Models.Rom
         public uint Addr { get; set; } = 0;
         public uint Marker { get; set; } = 0;
         public long Hash { get; set; } = 0;
+
+        // Store the instrument type, list index, and key region (if any)
+        public string InstrumentType { get; set; } = null;
+        public int ListIndex { get; set; } = -1;
+        public string KeyRegion { get; set; } = null;
     }
 }
