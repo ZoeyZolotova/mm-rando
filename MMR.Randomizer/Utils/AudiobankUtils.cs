cfb07d90424fea1ba605ce11c7c861db4987c325
Crin <usanimere@outlook.com>
2025-05-03 15:16:45 -0700

    Add a little bit more documentation

--- MMR.Randomizer/Utils/AudiobankUtils.cs
+++ MMR.Randomizer/Utils/AudiobankUtils.cs
@@ -6,6 +6,8 @@ namespace MMR.Randomizer.Utils
 {
     public class AudiobankUtils
     {
+        // Parses through a binary instrument bank (.zbank, decompressed) file and stores data
+
         public interface ISample
         {
             uint Address { get; set; }
@@ -14,6 +16,7 @@ namespace MMR.Randomizer.Utils
 
         // Should only need the sample offsets, and int should be fine for bank addresses
         // because there shouldn't be a bank out there longer than 0x7FFFFFFF bytes
+
         public class Audiobank
         {
             public List<Instrument> Instruments = new();
@@ -22,10 +25,14 @@ namespace MMR.Randomizer.Utils
 
             public Audiobank(byte[] metadata, byte[] bankData)
             {
+                // The metadata stored in SequenceSoundSampleBinaryData is not the full 0x10 bytes
+                // it's the .bankmeta from the music file.
+
                 int numInsts = metadata[4];
                 int numDrums = metadata[5];
                 int numEffects = BinaryPrimitives.ReadUInt16BigEndian(metadata.AsSpan(6, 2));
 
+                // Find all the drums and instantiate them
                 uint drumListAddr = BinaryPrimitives.ReadUInt32BigEndian(bankData.AsSpan(0, 4));
                 for (int i = 0; i < numDrums; i++)
                 {
@@ -35,6 +42,7 @@ namespace MMR.Randomizer.Utils
                     Drums.Add(drum);
                 }
 
+                // Find all the effects and instantiate them
                 uint effectListAddr = BinaryPrimitives.ReadUInt32BigEndian(bankData.AsSpan(4, 4));
                 for (int i = 0; i < numEffects; i++)
                 {
@@ -43,6 +51,7 @@ namespace MMR.Randomizer.Utils
                     Effects.Add(effect);
                 }
 
+                // Find all the instruments and instantiante them
                 for (int i = 0; i < numInsts; i++)
                 {
                     uint offset = 0x08 + (uint)(4 * i);
@@ -54,6 +63,9 @@ namespace MMR.Randomizer.Utils
 
             public List<ISample> GetBankSamples()
             {
+                // This runs through all the instruments, drums, and effects and adds any
+                // sample structs it finds to a list so the addresses can be searched
+
                 List<ISample> allSamples = new();
 
                 foreach (var instrument in Instruments)
@@ -62,8 +74,10 @@ namespace MMR.Randomizer.Utils
                     {
                         if (instrument.LowSample != null)
                             allSamples.Add(instrument.LowSample);
+
                         if (instrument.PrimSample != null)
                             allSamples.Add(instrument.PrimSample);
+
                         if (instrument.HighSample != null)
                             allSamples.Add(instrument.HighSample);
                     }
@@ -87,6 +101,9 @@ namespace MMR.Randomizer.Utils
 
         public class Sample<TParent> : ISample
         {
+            // The parent object is stored for fallback to get the index value in Instruments, Drums, or Effects
+            // The bitfield is ignored because it is unneeded currently
+
             public TParent Parent;
             public uint BankOffset {  get; set; }
             public uint Address {  get; set; }
@@ -104,6 +121,8 @@ namespace MMR.Randomizer.Utils
 
         public class Instrument
         {
+            // Gets data from an Instrument struct in an instrument bank
+
             public int InstrumentId {  get; set; }
             public uint LowSampleAddress { get; set; }
             public uint PrimSampleAddress { get; set; }
@@ -129,6 +148,8 @@ namespace MMR.Randomizer.Utils
 
         public class Drum
         {
+            // Gets data from a Drum struct in an instrument bank
+
             public int DrumId { get; set; }
             public uint SampleAddress { get; set; }
             public Sample<Drum> Sample { get; set; } = null;
@@ -146,6 +167,8 @@ namespace MMR.Randomizer.Utils
 
         public class Effect
         {
+            // Gets data from a TunedSample struct in the Effects list in an instrument bank
+
             public int EffectId { get; set; }
             public uint SampleAddress { get; set; }
             public Sample<Effect> Sample { get; set; } = null;
