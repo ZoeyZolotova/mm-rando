4f1c1e4777286e5a9a489a7294c7351bd7f87c7f
Crin <usanimere@outlook.com>
2025-05-04 15:17:09 -0700

    Store the name of the parent file
    If an error occurs during injection, then the problem file will be known

--- MMR.Randomizer/Models/Rom/SequenceSoundSampleBinaryData.cs
+++ MMR.Randomizer/Models/Rom/SequenceSoundSampleBinaryData.cs
@@ -1,16 +1,17 @@
-﻿
+
 namespace MMR.Randomizer.Models.Rom
 {
     public class SequenceSoundSampleBinaryData
     {
-        public byte[] BinaryData { get; set; } = null;
-        public uint Addr { get; set; } = 0;
-        public uint Marker { get; set; } = 0;
+        public byte[] BinaryData { get; set; }
+        public uint Addr { get; set; }
+        public uint Marker { get; set; }
         public long Hash { get; set; } = 0;
 
         // Store the instrument type, list index, and key region (if any)
-        public string InstrumentType { get; set; } = null;
+        public string ParentFile { get; set; }
+        public string InstrumentType { get; set; }
         public int ListIndex { get; set; } = -1;
-        public string KeyRegion { get; set; } = null;
+        public string KeyRegion { get; set; }
     }
 }
