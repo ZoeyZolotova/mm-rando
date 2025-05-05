cd01209d7fdaddd381a70a9d11ef16bfb6215a9a
Crin <usanimere@outlook.com>
2025-05-04 18:32:23 -0700

    Better unmatched audio sample error message

--- MMR.Randomizer/Utils/SequenceUtils.cs
+++ MMR.Randomizer/Utils/SequenceUtils.cs
@@ -1722,7 +1722,7 @@ namespace MMR.Randomizer.Utils
                         }
 
                         if (sampleBankAddress == 0)
-                            throw new Exception($"Error: Audio sample address is still 0 for sample in song: '{sample.ParentFile}'");
+                            throw new Exception($"Error: Could not match any sample address to audio sample's address in the instrument bank for song: '{sample.ParentFile}'");
 
                         // Replace the sample struct's address with the correct address
                         // The first 4 bytes are a bitfield, so add 4 to the index
