c7d6e5545f0bac02eee40e8e36cad800bfb2979a
Crin <usanimere@outlook.com>
2025-05-08 17:42:55 -0700

    Improve SequenceSoundSampleBinaryData.cs documentation

--- MMR.Randomizer/Models/Rom/SequenceSoundSampleBinaryData.cs
+++ MMR.Randomizer/Models/Rom/SequenceSoundSampleBinaryData.cs
@@ -1,6 +1,9 @@
 
 namespace MMR.Randomizer.Models.Rom
 {
+    /// <summary>
+    /// Represents a binary Zelda64 ADPCM audio sample file.
+    /// </summary>
     public class SequenceSoundSampleBinaryData
     {
         public byte[] BinaryData { get; set; }
@@ -10,8 +13,17 @@ namespace MMR.Randomizer.Models.Rom
 
         // Store the instrument type, list index, and key region (if any)
         public string ParentFile { get; set; }
+        /// <summary>
+        /// The type of instrument the sample is assigned to.
+        /// </summary>
         public string InstrumentType { get; set; }
+        /// <summary>
+        /// The index of the parent structure in the instrument, drum, or effect list.
+        /// </summary>
         public int ListIndex { get; set; } = -1;
+        /// <summary>
+        /// The key region of the sample struct in the instrument struct.
+        /// </summary>
         public string KeyRegion { get; set; }
     }
 }
