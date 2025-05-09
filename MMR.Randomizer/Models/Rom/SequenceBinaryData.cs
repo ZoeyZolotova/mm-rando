9f47a3351fffdadf8c3448cb3c38a1721dbbf0d5
Crin <usanimere@outlook.com>
2025-05-08 18:37:12 -0700

    Update SequenceBinaryData.cs

--- MMR.Randomizer/Models/Rom/SequenceBinaryData.cs
+++ MMR.Randomizer/Models/Rom/SequenceBinaryData.cs
@@ -1,12 +1,14 @@
-﻿
+
 namespace MMR.Randomizer.Models.Rom
 {
 
     public class SequenceBinaryData
     {
-        public byte[] SequenceBinary { get; set; } = null; // binary copy of the zsequence
+        /// <summary>
+        /// A bytearray copy of a sequence file's raw binary data.
+        /// </summary>
+        public byte[] SequenceData { get; set; } = null;
         public InstrumentSetInfo InstrumentSet { get; set; } = null;
-        public byte[] FormMask { get; internal set; }
+        public byte[] Formmask { get; internal set; }
     }
 }
-
