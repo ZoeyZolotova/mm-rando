a651715a39ba828ef6c575ac351eb161939b3af0
Crin <usanimere@outlook.com>
2025-05-08 18:36:38 -0700

    Update SequenceInfo.cs

--- MMR.Randomizer/Models/Rom/SequenceInfo.cs
+++ MMR.Randomizer/Models/Rom/SequenceInfo.cs
@@ -15,39 +15,15 @@ namespace MMR.Randomizer.Models.Rom
         public string Filename => Path.Combine(Directory, Name);
         public string Filepath { get; set; } // For music cache
         public int Replaces { get; set; } = -1;
+        /// <summary>
+        /// Majora's Mask sequence ID that the sequence represents.
+        /// </summary>
         public int SeqId { get; set; } = -1; // MM_seq -> SeqId
         public List<int> Categories { get; set; } = new List<int>();
         public int Instrument { get; set; }
-        public List<SequenceBinaryData> SequenceBinaryList { get; set; }
+        public SequenceBinaryData SequenceBinary { get; set; }
         public int PreviousSlot { get; set; } = -1;
         public List<SequenceSoundSampleBinaryData> InstrumentSamples { get; set; } = null;
-
-        public void ClearUnavailableBanks()
-        {
-            /// custom instrument sets relies on overwriting a vanilla bank with a new bank that has the old instruments, with additions
-            /// this feature does NOT support two banks stuffing into one slot, first bank to take a slot claims it and other songs cannot use for banks
-            /// this function should clear away banks that are unavailable because they are already taken
-
-            // get list of banks that: their slot has not been modified, or their bank is already used by another song and can be reused
-            this.SequenceBinaryList = this.SequenceBinaryList.FindAll(u => u.InstrumentSet == null
-                                                                        || (RomData.InstrumentSetList[u.InstrumentSet.BankSlot].Modified == 0
-                                                                        || (u.InstrumentSet.Hash != 0
-                                                                        && u.InstrumentSet.Hash == RomData.InstrumentSetList[u.InstrumentSet.BankSlot].Hash)));
-        }
-
-        public bool CheckAvailableBanks()
-        {
-            // get list of banks that: their slot has not been modified, or their bank is already used by another song and can be reused
-            var banks = this.SequenceBinaryList.FindAll(u => u.InstrumentSet == null
-                                                          || (RomData.InstrumentSetList[u.InstrumentSet.BankSlot].Modified == 0
-                                                          || (u.InstrumentSet.Hash != 0
-                                                          && u.InstrumentSet.Hash == RomData.InstrumentSetList[u.InstrumentSet.BankSlot].Hash)));
-            if (banks.Count == 0)
-            {
-                return false;
-            }
-            return true;
-        }
 
     }
 }
