57e5160d82b077d16b1bf57ebb240b00b27096a6
Crin <usanimere@outlook.com>
2025-05-08 17:40:41 -0700

    Improve Addresses.cs documentation.

--- MMR.Randomizer/Constants/Addresses.cs
+++ MMR.Randomizer/Constants/Addresses.cs
@@ -1,5 +1,10 @@
+using MMR.Randomizer.Models.Rom;
+
 namespace MMR.Randomizer.Constants
 {
+    /// <summary>
+    /// Stores the ROM addresses of audio-related MMFiles.
+    /// </summary>
     public static class Addresses
     {
         // these are addresses for tables that point to the specific objects for audio
@@ -15,15 +20,45 @@ namespace MMR.Randomizer.Constants
         // TODO add audiobank and soundbank pointers
 
         // Majora's Mask Audiobin
+        /// <summary>
+        /// The address of the 'audiobank index' on the ROM.
+        /// </summary>
         public const int AUDIOBANK_INDEX_ADDR    = 0x00C776C0;
+        /// <summary>
+        /// The size of the 'audiobank index' in the 'code' MMFile.
+        /// </summary>
         public const int AUDIOBANK_INDEX_SIZE    = 0x000002A0;
+
+        /// <summary>
+        /// The address of the 'audiobank' MMFile.
+        /// </summary>
         public const int AUDIOBANK_ADDR          = 0x00020700;
+        /// <summary>
+        /// The size of the 'audiobank' MMFile.
+        /// </summary>
         public const int AUDIOBANK_SIZE          = 0x000263F0;
+
+        /// <summary>
+        /// The address of the 'audiotable index' on the ROM.
+        /// </summary>
         public const int AUDIOTABLE_INDEX_ADDR   = 0x00C78380;
+        /// <summary>
+        /// The size of the 'audiotable index' in the 'code' MMFile.
+        /// </summary>
         public const int AUDIOTABLE_INDEX_SIZE   = 0x000002A0;
+
+        /// <summary>
+        /// The address of the 'audiotable' MMFile.
+        /// </summary>
         public const int AUDIOTABLE_ADDR         = 0x00097F70;
+        /// <summary>
+        /// The size of the 'audiotable' MMFile.
+        /// </summary>
         public const int AUDIOTABLE_SIZE         = 0x00548770;
 
+        /// <summary>
+        /// The address of the 'code' MMFile.
+        /// </summary>
         public const int CODE_ADDR               = 0x00B3C000;
     }
 }
