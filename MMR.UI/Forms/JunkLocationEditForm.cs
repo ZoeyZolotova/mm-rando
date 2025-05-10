f872ed15de18a5402c41fe2df838a022a682b1f3
Crin <usanimere@outlook.com>
2025-05-09 17:34:24 -0700

    Update JunkLocationEditForm.cs

--- MMR.UI/Forms/JunkLocationEditForm.cs
+++ MMR.UI/Forms/JunkLocationEditForm.cs
@@ -8,9 +8,11 @@ using System.Windows.Forms;
 using MMR.Randomizer.Extensions;
 using MMR.Common.Extensions;
 using MMR.Randomizer.Attributes.Setting;
+using System.Runtime.Versioning;
 
 namespace MMR.UI.Forms
 {
+    [SupportedOSPlatform("windows")]
     public partial class JunkLocationEditForm : Form
     {
         private readonly List<Item> _junkLocations;
