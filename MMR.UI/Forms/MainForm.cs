2c1db5a510f55efde37059f9d83de997343234a6
Crin <usanimere@outlook.com>
2025-06-11 05:46:34 -0700

    Update MainForm.cs

--- MMR.UI/Forms/MainForm.cs
+++ MMR.UI/Forms/MainForm.cs
@@ -1208,7 +1208,7 @@ namespace MMR.UI.Forms
             var button = (Button)sender;
             dlg.Color = button.BackColor;
 
-            var result = cTunic.ShowDialog();
+            var result = dlg.ShowDialog();
             if (result == DialogResult.OK)
             {
                 _isUpdating = true;
